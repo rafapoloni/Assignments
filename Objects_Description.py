c47673e6a49d448a1608074d0cf081517501e748
rafapoloni <55499442+rafapoloni@users.noreply.github.com>
2019-09-26 12:30:01 -0400

    Add files via upload

---
+++ Objects_Description.py
@@ -0,0 +1,40 @@
+# object 1
+MyPhone = {
+	"model" : "Samsung Galaxy S9",
+	"color" : "black",
+	"screen size" : "5.8 in",
+	"weight" : "5.75 oz",
+	"resolution" : "570 ppi",
+	"cameras" : "3",
+	"storage capacity" : "64 Gb",
+	" water resistant" : "yes"
+}
+
+# object 2
+MyEarphones = {
+	"brand" : "apple"
+	"color" : "white"
+	"has wires" : "yes"
+}
+
+# object 3
+PoonamsBook = {
+	"object" : "Book", 
+	"title" : "GamechangeR", 
+	"color" :"black and white", 
+	"lenght" : "15 pages", 
+	"size" : "palm size" 
+}
+
+# object 4
+Pen = {
+    "parts": ["body", "cap"],
+    "body": {
+        "draws": true,
+        "colour": "red"
+    },
+    "cap": {
+        "draws": false,
+        "colour": "black"
+    }
+}
